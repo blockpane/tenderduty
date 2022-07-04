9fd0f49a13468caf90f011779b75ed619e17b012
Todd Garrison <todd@blockpane.com>
2022-07-04 02:49:36 -0600

    working, but not right.

--- main.go
+++ main.go
@@ -13,15 +13,39 @@ import (
 var defaultConfig []byte
 
 func main() {
-	var configFile, stateFile string
-	var dumpConfig bool
+	var configFile, encryptedFile, stateFile, password string
+	var dumpConfig, encryptConfig, decryptConfig bool
 	flag.StringVar(&configFile, "f", "config.yml", "configuration file to use")
+	flag.StringVar(&encryptedFile, "encrypted-config", "config.yml.asc", "encrypted config file, only valid with -encrypt or -decrypt flag")
 	flag.StringVar(&stateFile, "state", ".tenderduty-state.json", "file for storing state between restarts")
+	flag.StringVar(&password, "password", "", "password to use for encrypting/decrypting the config, if unset will prompt, also can use ENV var 'PASSWORD'")
 	flag.BoolVar(&dumpConfig, "example-config", false, "print the an example config.yml and exit")
+	flag.BoolVar(&encryptConfig, "encrypt", false, "encrypt the file specified by -f to -encrypted-config")
+	flag.BoolVar(&decryptConfig, "decrypt", false, "decrypt the file specified by -encrypted-config to -f")
 	flag.Parse()
 
 	if dumpConfig {
 		fmt.Println(string(defaultConfig))
+		os.Exit(0)
+	}
+
+	if encryptConfig || decryptConfig {
+		if password == "" {
+			if os.Getenv("PASSWORD") != "" {
+				password = os.Getenv("PASSWORD")
+			} else {
+				// TODO: prompt for password
+			}
+		}
+		var e error
+		if encryptConfig {
+			e = td2.EncryptedConfig(configFile, encryptedFile, password, false)
+		} else {
+			e = td2.EncryptedConfig(configFile, encryptedFile, password, true)
+		}
+		if e != nil {
+			log.Fatalln(e)
+		}
 		os.Exit(0)
 	}
 
