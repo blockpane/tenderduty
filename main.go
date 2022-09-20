002c6677e3d3fa3aca2dedc74753612674fff420
Chill Validation <blockhead@chillvalidation.com>
2022-09-20 20:28:53 +0900

    Enable chain specific configuration files

--- main.go
+++ main.go
@@ -4,19 +4,21 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
-	td2 "github.com/blockpane/tenderduty/v2/td2"
 	"log"
 	"os"
+
+	td2 "github.com/blockpane/tenderduty/v2/td2"
 )
 
 //go:embed example-config.yml
 var defaultConfig []byte
 
 func main() {
-	var configFile, stateFile string
+	var configFile, chainConfigDirectory, stateFile string
 	var dumpConfig bool
 	flag.StringVar(&configFile, "f", "config.yml", "configuration file to use")
 	flag.StringVar(&stateFile, "state", ".tenderduty-state.json", "file for storing state between restarts")
+	flag.StringVar(&chainConfigDirectory, "cc", "chains.d", "directory containing additional chain specific configurations")
 	flag.BoolVar(&dumpConfig, "example-config", false, "print the an example config.yml and exit")
 	flag.Parse()
 
@@ -25,7 +27,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	err := td2.Run(configFile, stateFile)
+	err := td2.Run(configFile, stateFile, chainConfigDirectory)
 	if err != nil {
 		log.Println(err.Error(), "... exiting.")
 	}
